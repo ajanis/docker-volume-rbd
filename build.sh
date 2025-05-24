4d76dd149c90b56f18b932d67314d5182b66c734
Alan Janis <alan.janis@gmail.com>
2025-05-24 02:23:32 -0600

    Rewrite build script to accept args for name and tag, print commands as executed and log all commands and output

--- build.sh
+++ build.sh
@@ -1,26 +1,70 @@
 #!/bin/bash
 
-version=v19.2
-pluginname=ajanis/rbd
-pluginnametagged=$pluginname:$version
-echo $pluginnametagged
+# Default values
+pluginTag="v19.2"
+pluginName="ajanis/rbd"
 
-docker plugin disable $pluginnametagged -f
-docker plugin rm $pluginnametagged -f
-sudo rm -rf plugin
+# Parse options with getopt (supports both short and long)
+OPTIONS=$(getopt --options t:n: --long tag:,name: --name "$0" -- "$@")
+if [[ $? -ne 0 ]]; then
+  exit 2
+fi
 
+eval set -- "$OPTIONS"
+
+# Process options
+while true; do
+  case "$1" in
+    -v|--pluginTag)
+      pluginTag="$2"
+      shift 2
+      ;;
+    -n|--name)
+      pluginName="$2"
+      shift 2
+      ;;
+    --)
+      shift
+      break
+      ;;
+    *)
+      echo "Unexpected option: $1"
+      exit 3
+      ;;
+  esac
+done
+
+# Log setup
+logfile="plugin_build_$(date +%Y%m%d_%H%M%S).log"
+
+# Redirect all output to log and console
+exec > >(tee -a "$logfile") 2>&1
+
+# Print commands as they run
+set -x
+
+pluginNameTagged="${pluginName}:${pluginTag}"
+echo "Building plugin: ${pluginNameTagged}"
+
+# Cleanup
+docker plugin disable "${pluginNameTagged}" -f
+docker plugin rm "${pluginNameTagged}" -f
+sudo rm -rf rootfs
+
+# Rebuild plugin
 git pull
-docker build . -t $pluginnametagged
+docker build . -t "${pluginNameTagged}"
 
-id=$(docker create $pluginnametagged true)
-mkdir -p plugin/rootfs
-cp config.json plugin/
-docker export "$id" -o plugin/container.tar
-sudo tar --extract -f plugin/container.tar --directory plugin/rootfs
-rm plugin/container.tar
+id=$(docker create "${pluginNameTagged}" true)
+mkdir rootfs
+docker export "$id" | tar -x -C rootfs
 docker rm -vf "$id"
-docker rmi $pluginnametagged
+docker rmi "${pluginNameTagged}"
 
-sudo docker plugin create $pluginnametagged plugin/
-docker plugin enable $pluginnametagged
+# Create and enable plugin
+sudo docker plugin create "${pluginNameTagged}" .
+docker plugin enable "${pluginNameTagged}"
+
+# List Plugins
 docker plugin ls
+
